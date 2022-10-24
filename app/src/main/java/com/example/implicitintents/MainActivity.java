1d5dc543d9c6df432ac3c64e0cea9e2c64fb12fc
Ahzi25 <70834999+Ahzi25@users.noreply.github.com>
2022-10-24 17:28:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.implicitintents;
+
+import android.content.Intent;
+import android.net.Uri;
+import androidx.core.app.ShareCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText mShareTextEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edtittext);
+        mShareTextEditText = findViewById(R.id.share_edittext);
+    }
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri website = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, website);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+        intent.setPackage("com.google.android.apps.maps");
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void shareText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(R.string.edittext_share)
+                .setText(txt)
+                .startChooser();
+    }
+
+    public void takeAPic(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+}
